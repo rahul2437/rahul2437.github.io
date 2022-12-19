ff63712dbfa1711dfab08a970c7b443e857427f8
Rahul Sheelavantar <rahulsheelavantar@gmail.com>
2022-12-19 17:28:24 +0530

    Responsive

--- src/portfolio.js
+++ src/portfolio.js
@@ -22,6 +22,18 @@ const about = {
   },
 }
 
+const myStory = {
+  description: `I remember the first time I used basic programming in school and wrote simple programs. It was exciting to see your sentences come alive on the website. I then began learning C++ and JAVA. The process of solving problems in C++ and Java ignited a spark in my head and made me contemplate it as a career. My curiosity grew and I wanted to know how software and applications worked. And so I decided to pursue B.Tech. Unfortunately, I could not get into Computer Science and ended up joining Mechanical Engineering.
+  After my graduation, while working in the automobile industry I realized I want to get into Information Technology. I then enrolled at Masai School for a course on Full Stack Development and currently aspiring to be a Full Stack Web Developer.
+  
+  I am a quick learner and have good problem-solving skills. I am also interested in Design Thinking. I am adept in Java, HTML, CSS, Javascript, C++, ReactJS, MongoDB, NodeJS, REST APIs, and Git/Github.
+  
+  Apart from web development, I am interested in traveling, driving, sketching, and Formula 1.
+  
+  You can check out my work here https://github.com/rahul2437/
+  You may reach out to me at rahulsheelavantar@gmail.com`,
+}
+
 const projects = [
   {
     name: 'Kayak.com',
